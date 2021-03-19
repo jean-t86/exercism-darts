93df91cf52dbe4af181f44d03a606968b69a44f9
J.Tadebois <jpm.tadebois@gmail.com>
2021-03-19 18:20:55 +0400

    Add completed solution

--- src/main/kotlin/Darts.kt
+++ src/main/kotlin/Darts.kt
@@ -1,6 +1,35 @@
+import kotlin.math.pow
+import kotlin.math.sqrt
+
 object Darts {
 
-    fun score(x: Any, y: Any /* choose proper types! */): Int {
-        TODO("Implement this function to complete the task")
+    fun score(x: Double, y: Double): Int {
+        if (x == -3.6 && y == -3.6) {
+            println()
+        }
+        val distanceFromCentre = distanceFromCentre(x, y)
+        return when {
+            distanceFromCentre > 10.0 -> 0
+            distanceFromCentre in 5.01..10.0 -> 1
+            distanceFromCentre in 1.01..5.0 -> 5
+            distanceFromCentre in 0.0..1.0 -> 10
+            else -> 10
+        }
+    }
+
+    fun score(x: Int, y: Int): Int {
+        return score(x.toDouble(), y.toDouble())
+    }
+
+    fun score(x: Double, y: Int): Int {
+        return score(x, y.toDouble())
+    }
+
+    private fun distanceFromCentre(x: Double, y: Double): Double {
+        val centreX = 0.0
+        val centreY = 0.0
+        val distanceX = (x - centreX).pow(2.0)
+        val distanceY = (y - centreY).pow(2.0)
+        return sqrt(distanceX + distanceY)
     }
 }
